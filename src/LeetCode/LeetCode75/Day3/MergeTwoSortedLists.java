4dbe1184152140e19880c900f3f0fdd1e2363470
XAfker <podlinskibartosz@gmail.com>
2022-12-08 03:24:14 +0100

    Commit

--- src/LeetCode/LeetCode75/Day3/MergeTwoSortedLists.java
+++ src/LeetCode/LeetCode75/Day3/MergeTwoSortedLists.java
@@ -4,6 +4,15 @@ public class MergeTwoSortedLists {
 
 
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null && list2 == null) {
+            return null;
+        }
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
         ListNode head;
         if(list1.val <= list2.val){
             head = list1;
