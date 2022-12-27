74580ddfdc59a55c604f3bc284656a8fac6c3dc1
XAfker <podlinskibartosz@gmail.com>
2022-12-27 01:22:53 +0100

    Commit

---
+++ src/LeetCode/LeetCode75/Day10/ClimbingStairs.java
@@ -0,0 +1,17 @@
+package LeetCode.LeetCode75.Day10;
+
+public class ClimbingStairs {
+
+    public int climbStairs(int n) {
+        int[] arr = new int[n + 1];
+        arr[0] = 0;
+        arr[1] = 1;
+        if(n > 1)
+            arr[2] = 2;
+
+        for(int i = 3; i < arr.length; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        return arr[n];
+    }
+}
